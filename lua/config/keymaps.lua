9a0b0210f75e652e595ce903d414ab16633f1206
haesbaerteduardo <eduardo.haesbaert@leanix.net>
2024-09-20 16:46:21 +0200

    add mapping to open commit from git blame

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,10 @@ vim.api.nvim_set_keymap(
   '<cmd>lua require("telescope.builtin").live_grep()<CR>',
   { noremap = true, silent = true, desc = "Grep (Root Dir)" }
 )
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<Leader>go",
+  "<cmd>GitBlameOpenCommitURL<CR>",
+  { noremap = true, silent = true, desc = "Grep (Root Dir)" }
+)
