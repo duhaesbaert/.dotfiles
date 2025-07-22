bc2f00ac7e7e179fedc45f9953019102c3fe35bb
haesbaerteduardo <eduardo.haesbaert@leanix.net>
2025-07-22 11:27:22 +0200

    fix jdtls startup

--- lua/plugins/lsp/jdtls.lua
+++ lua/plugins/lsp/jdtls.lua
@@ -12,7 +12,6 @@ return {
   end,
 }
 ]]
-
 return {
   {
     "neovim/nvim-lspconfig",
@@ -72,21 +71,15 @@ return {
               -- vim.lsp.set_log_level('DEBUG')
               local workspace_dir = "/home/jake/.workspace/" .. project_name -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
               local config = {
-                -- The command that starts the language server
-                -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
                 cmd = {
 
-                  "java", -- or '/path/to/java17_or_newer/bin/java'
-                  -- depends on if `java` is in your $PATH env variable and if it points to the right version.
-
+                  "java",
                   "-javaagent:/home/jake/.local/share/java/lombok.jar",
-                  -- '-Xbootclasspath/a:/home/jake/.local/share/java/lombok.jar',
                   "-Declipse.application=org.eclipse.jdt.ls.core.id1",
                   "-Dosgi.bundles.defaultStartLevel=4",
                   "-Declipse.product=org.eclipse.jdt.ls.core.product",
                   "-Dlog.protocol=true",
                   "-Dlog.level=ALL",
-                  -- '-noverify',
                   "-Xms1g",
                   "--add-modules=ALL-SYSTEM",
                   "--add-opens",
@@ -95,28 +88,14 @@ return {
                   "java.base/java.lang=ALL-UNNAMED",
                   "-jar",
                   vim.fn.glob("/usr/share/java/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-                  -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
-                  -- Must point to the                                                     Change this to
-                  -- eclipse.jdt.ls installation                                           the actual version
-
                   "-configuration",
                   "/usr/share/java/jdtls/config_linux",
-                  -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
-                  -- Must point to the                      Change to one of `linux`, `win` or `mac`
-                  -- eclipse.jdt.ls installation            Depending on your system.
-
-                  -- See `data directory configuration` section in the README
                   "-data",
                   workspace_dir,
                 },
 
-                -- This is the default if not provided, you can remove it. Or adjust as needed.
-                -- One dedicated LSP server & client will be started per unique root_dir
                 root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" }),
 
-                -- Here you can configure eclipse.jdt.ls specific settings
-                -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-                -- for a list of options
                 settings = {
                   java = {},
                 },
